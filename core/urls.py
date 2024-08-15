735ef6282d8fefd3b0e0c3c40b0eca214a9ec6d8
cjbwebdev <curtisbooth87@yahoo.com>
2024-08-15 17:21:22 +0100

    enabled conversations to be read in admin section

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from .views import user_logout
 
 from . import views
 from .forms import LoginForm
@@ -11,4 +12,5 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm),name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
